82ca8641b95122dac809465ed73468bb909cb5aa
Lars Bättig <me@l4rs.net>
2021-12-05 11:17:51 +0100

    error handling

--- worker_http.py
+++ worker_http.py
@@ -7,11 +7,11 @@ devices, http_devices = loadDevices('http')
 while True:
     for d in http_devices:
         print(d)
-        e, out = devices[d].getState()
+        out = devices[d].getState()
 
-        if not e:
-            print('ERROR - {} - getState() was not sucessfull'.format(d))
-            continue
+        #if not e:
+        #    print('ERROR - {} - getState() was not sucessfull'.format(d))
+        #    continue
 
         print('INFO - {} - getState(): {}'.format(d, out))
 
