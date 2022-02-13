e5444a45e66cd3aea1e10ca8a5bf47a29ff57570
Lars Bättig <me@l4rs.net>
2022-02-13 14:07:07 +0100

    adds ui

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import yaml
 import uuid
 import logging
 import tempfile
+import requests
 
 from flask import Flask
 #from flask import _app_ctx_stack
@@ -18,14 +19,16 @@ from flask import send_from_directory
 from flask import url_for
 from flask import redirect
 from flask_cors import CORS
-#from functools import wraps
+from flask import Response
+from flask import stream_with_context
+from flask import render_template
 
 from utils import *
 
 ##
 # init app
 ##
-app = Flask(__name__)
+app = Flask(__name__, template_folder='ui')
 app.logger.setLevel(logging.INFO)
 CORS(app)
 
@@ -98,6 +101,53 @@ if app.config['DEBUG']:
 ##
 # routes
 ##
+# ui files
+@app.route('/')
+def ui_index():
+    devices, _ = loadDevices()
+    return render_template("index.html", devices=devices)
+
+@app.route('/details/<device>')
+def ui_details(device):
+    metrics = []
+    from_data = request.args.get('from')
+    if not from_data:
+        from_data = "-1days"
+
+    url = "{}/metrics/find?query={}.{}.*".format(
+        config['metrics']['render_api_base_url'],
+        config['metrics']['prefix'],
+        device
+    )
+
+    r = requests.get(url, timeout=3)
+    #if r.status_code != 200:
+    #    data = {
+    #        "status": "error",
+    #        "data": r.text()
+    #    }
+
+    if r.status_code == 200:
+        for m in r.json():
+            metrics.append(m['text'])
+
+    device_data = loadDevice(device)
+
+    return render_template(
+        "details.html",
+        device = device,
+        metrics = metrics,
+        from_data = from_data,
+        device_data = device_data
+    )
+
+@app.route('/<path:filename>')
+def ui(filename=None):
+    if filename == None:
+        filename = 'index.html'
+
+    return send_from_directory('ui/', filename)
+
 @app.route('/api/v1/devices')
 def show_devices():
     data = []
@@ -106,7 +156,7 @@ def show_devices():
     for d in devices.keys():
         data.append(devices[d].__dict__)
 
-    return jsonify(data)
+    return jsonify(list(devices.keys()))
 
 @app.route('/api/v1/devices/<device>', methods = [ 'GET', 'POST' ])
 def show_device(device):
@@ -142,20 +192,48 @@ def show_device(device):
 
         return jsonify(device.action(action, msg))
 
-# ui files
-#@app.route('/ui/stats')
-#@app.route('/ui/list')
-#@app.route('/ui/send')
-#@app.route('/ui/login')
-#@app.route('/ui/logout')
-#@app.route('/ui/<path:filename>')
-#@app.route('/ui')
-#def ui(filename=None):
-#    if filename == None:
-#        filename = 'index.html'
-#
-#    return send_from_directory('ui/dist/', filename)
-#
+@app.route('/api/v1/devices/<device>/metrics', methods = [ 'GET' ])
+def device_metrics(device):
+    url = "{}/metrics/find?query={}.{}.*".format(
+        config['metrics']['render_api_base_url'],
+        config['metrics']['prefix'],
+        device
+    )
+
+    r = requests.get(url, timeout=3)
+    if r.status_code != 200:
+        data = {
+            "status": "error",
+            "data": r.text()
+        }
+
+    if r.status_code == 200:
+        data = []
+
+        for m in r.json():
+            data.append(m['text'])
+
+    return jsonify(data)
+
+@app.route('/api/v1/devices/<device>/metrics/<metric>', methods = [ 'GET' ])
+def device_metrics_render(device, metric):
+    from_data = request.args.get('from')
+
+    if not from_data:
+        from_data = "-1days"
+
+    url = "{}/render/?target=aliasByNode(cactiStyle(aliasByNode({}.{}.{},-1)),1)&from={}&height=400&width=800&title={}&bgcolor=white&fgcolor=black&drawNullAsZero=false&lineMode=connected&colorList=green&yMin=0".format(
+        config['metrics']['render_api_base_url'],
+        config['metrics']['prefix'],
+        device,
+        metric,
+        from_data,
+        metric
+    )
+
+    r = requests.get(url, stream = True)
+    return Response(stream_with_context(r.iter_content()), content_type = r.headers['content-type'])
+
 #@app.route('/api/login', methods = ['POST'])
 #@auth
 #def login():
