6a98ac022ac5a7d4d848873b282da6ff44bdb6d7
Lars Bättig <me@l4rs.net>
2023-10-17 10:31:43 +0200

    removes office2 from office lights

--- scenes/office_light.py
+++ scenes/office_light.py
@@ -7,8 +7,5 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from utils import *
 
 office1 = loadDevice('office1')
-office2 = loadDevice('office2')
-
 office1.setState('toggle')
-office2.setState('toggle')
 
